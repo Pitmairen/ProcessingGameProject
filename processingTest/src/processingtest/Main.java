ed88340f037aa7c39bea9eb6300788415db0c46f
Kristian Honningsvag <kristian@digitalunion.net>
2016-01-07 09:14:51 +0100

    Uploaded core.jar. Created testing project.

---
+++ processingTest/src/processingtest/Main.java
@@ -0,0 +1,15 @@
+package processingtest;
+
+/**
+ *
+ * @author Kristian Honningsvag.
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sketch.main("processingtest.Sketch");
+    }
+}
