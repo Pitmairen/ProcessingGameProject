45e43a0b617fe55daa245944fb7fcab4da5e1261
Tor-Martin Holen <tormx99@gmail.com>
2016-02-17 13:55:41 +0100

    Graphics for weapons on enemies
    I don't know how to get the rotation to work so the change in the code
    that displays the graphic is commented out.

--- src/main/java/backend/shipmodule/OffensiveModule.java
+++ src/main/java/backend/shipmodule/OffensiveModule.java
@@ -1,6 +1,7 @@
 package backend.shipmodule;
 
 import backend.actor.Actor;
+import processing.core.PImage;
 
 /**
  * Super class for all offensive ship modules.
@@ -9,6 +10,8 @@ import backend.actor.Actor;
  */
 public abstract class OffensiveModule extends ShipModule {
 
+    private PImage weapon;
+    
     public OffensiveModule(String name, Actor owner) {
         super(name, owner);
     }
@@ -19,6 +22,19 @@ public abstract class OffensiveModule extends ShipModule {
 
     @Override
     public void draw() {
+        
+    }
+    
+    /**
+     * Method for setting the offensive graphics for a enemy
+     * @param image 
+     */
+    public void setOffensiveModule(String image){
+        weapon = owner.getGuiHandler().loadImage(image);
+    }
+    
+    public PImage getWeapon(){
+        return weapon;
     }
 
 }
